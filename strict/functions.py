21f5db391139f6d81afaacdb7f92f3b551be34bd
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2018-08-26 22:29:14 +0000

    Added function prototype enforcement (and aaallmost at call-time).

--- strict/functions.py
+++ strict/functions.py
@@ -7,6 +7,7 @@ import inspect
 import textwrap
 import ast
 import typing
+import functools
 
 from .enums import Attribute
 from . import singletons
@@ -18,15 +19,52 @@ def register() -> None:
 
 class Prototype:
     __slots__ = ('margs', 'oargs', 'mkwargs', 'okwargs', 'args', 'kwargs')
-    margs: typing.Sequence[str]
-    oargs: typing.Mapping[str, object]
-    mkwargs: typing.Sequence[str]
-    okwargs: typing.Mapping[str, object]
+    margs: typing.Mapping[str, typing.Tuple[type]]
+    oargs: typing.Mapping[str, typing.Tuple[type, object]]
+    mkwargs: typing.Mapping[str, typing.Tuple[type]]
+    okwargs: typing.Mapping[str, typing.Tuple[type, object]]
     args: bool
     kwargs: bool
 
     def __init__(self, f: types.FunctionType):
-        pass
+        c = f.__code__
+        argnames = c.co_varnames[:c.co_argcount]
+        kwargnames = c.co_varnames[c.co_argcount :
+                                   c.co_argcount + c.co_kwonlyargcount]
+        try:
+            noargs = len(f.__defaults__)
+        except TypeError:
+            noargs = 0
+        self.margs = {k: (f.__annotations__[k],)
+                      for k in argnames[:-noargs]}
+        if noargs:
+            self.oargs = {k: (f.__annotations__[k], f.__defaults__[k])
+                          for k in argnames[-noargs:]}
+        else:
+            self.oargs = {}
+
+        try:
+            nokwargs = len(f.__kwdefaults__)
+        except TypeError:
+            nokwargs = 0
+        self.mkwargs = {k: (f.__annotations__[k],)
+                        for k in kwargnames[:-nokwargs]}
+        if nokwargs:
+            self.okwargs = {k: (f.__annotations__[k], f.__defaults__[k])
+                            for k in kwargnames[-nokwargs:]}
+        else:
+            self.okwargs = {}
+
+        self.args = c.co_flags | 0x0004 > 0
+        self.kwargs = c.co_flags | 0x0008 > 0
+
+    def __eq__(self, other):
+        return (self.margs == other.margs
+                and self.oargs == other.oargs
+                and self.mkwargs == other.mkwargs
+                and self.okwargs == other.okwargs
+                and self.args == other.args
+                and self.kwargs == other.kwargs)
 
 class FunctionDescriptor:
     def __init__(self, f):
@@ -34,7 +72,13 @@ class FunctionDescriptor:
                    f.__code__.co_varnames + ("return",)):
             raise ValueError("Your function needs annotations!")
 
-        # Create a copy of the function, preserving variable annotations
+        # Set prototype from f.__code__.co_varnames and __annotations__
+        self.prototype = Prototype(f)
+
+        # TODO: Create a copy of the function, preserving variable annotations
+        self.function = f
+        return
+
         try:
             source_code = inspect.getsource(f)
         except OSError:
@@ -42,6 +86,7 @@ class FunctionDescriptor:
                           category=RuntimeWarning, stacklevel=4)
             self.function = f
         else:
+            # TODO: Finish implementing this!
             tree = ast.parse(textwrap.dedent(source_code)).body[0]
 
             code = compile(self.get_body(f),
@@ -52,14 +97,20 @@ class FunctionDescriptor:
 
             self.function = f
 
-        # Set prototype from f.__code__.co_varnames and __annotations__
-        self.prototype = Prototype(f)
 
     def __get__(self, instance, type_):
+        @functools.wraps(self.function)
+        def f(*args, **kwargs):
+            mandatory = True
+            for i, arg in enumerate(args):
+                if mandatory:
+                    pass
         return self.function
 
     def __set__(self, instance, value):
-        raise NotImplementedError("I'll do this later!")
+        if self.prototype != Prototype(value):
+            raise ValueError("Conflicting prototype during "
+                             "function reassignment.")
 
     def __set_name__(self, owner, name):
         print("This is called!")
@@ -67,6 +118,7 @@ class FunctionDescriptor:
 
     @staticmethod
     def get_body(f: types.FunctionType):
+        raise ValueError("This is dangerously naive.")
         source_code = inspect.getsource(f)
 
         index = source_code.index('(')
